da6abef2be057a526e4ab4da143d63426685bfd6
Chayut <67853769+chaychun@users.noreply.github.com>
2026-03-28 10:39:44 +0700

    Remove voice capture component (#91)

--- src/playground/voice-capture/preview.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-export const frame = { minHeight: 480 };
-
-const VoiceCapture = dynamic(() => import("./voice-capture"));
-
-export default function Preview() {
-  return <VoiceCapture />;
-}
